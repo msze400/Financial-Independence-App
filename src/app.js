871f59515170f767ce341bb2949cbed6faddd3e5
Matt Sze <msze400@gmail.com>
2021-02-24 19:59:33 -0500

    toggle functioning correctly

--- src/app.js
+++ src/app.js
@@ -9,20 +9,43 @@ import StackedAreaSeries from './components/StackedAreaSeries.js';
 import Legend from './components/Legend.js';
 import Navbar from './components/Navbar.js';
 import SimpleModal from './components/Meme.js';
+import { render } from 'react-dom';
+import Button from '@material-ui/core/Button';
 
-const App = () => {
-    return (
-        <div id="App">
-            <Navbar />
-            <div id="Body">
-                <UserInput />
-                <Legend />
-                <BarSeries />
-                <StackedAreaSeries />
-                <SimpleModal />
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isHidden: true,
+        };
+    }
+
+    toggleHidden() {
+        this.setState({
+            isHidden: !this.state.isHidden,
+        });
+    }
+
+    render() {
+        // console.log('STATE', this.state);
+        return (
+            <div id="App">
+                <Navbar />
+                <div id="Body">
+                    <UserInput />
+                    <Legend />
+                    {this.state.isHidden ? <BarSeries /> : null}
+                    {!this.state.isHidden ? <StackedAreaSeries /> : null}
+                </div>
+                <div>
+                    <SimpleModal />
+                    <Button onClick={this.toggleHidden.bind(this)}>Toggle</Button>
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default App;
+// const mapStateToProps = (state) => state;
+// export default connect(mapStateToProps)(App);
--- src/components/Legend.js
+++ src/components/Legend.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { DiscreteColorLegend } from 'react-vis';
 
-const ITEMS = ['Initial Balance', 'Compound Interest Growth', 'Amount Contributed'];
+const ITEMS = ['Compound Interest Growth', 'Amount Contributed'];
 
 export default function DiscreteColorExample() {
     return <DiscreteColorLegend height={200} width={300} items={ITEMS} />;
