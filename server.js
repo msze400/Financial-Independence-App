39d2b584a29eb24455853f1a66f78f51cc0930ae
Matt Sze <msze400@gmail.com>
2021-02-27 10:04:00 -0500

    edit server file for heroku

--- server.js
+++ server.js
@@ -30,18 +30,18 @@ const init = async () => {
 
 const Sequelize = require('sequelize');
 const { STRING } = Sequelize;
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db');
+// const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db');
 
-const User = conn.define('user', {
-    name: STRING,
-});
+// const User = conn.define('user', {
+//     name: STRING,
+// });
 const syncAndSeed = async () => {
-    await conn.sync({ force: true });
-    await Promise.all([
-        User.create({ name: 'moe' }),
-        User.create({ name: 'larry' }),
-        User.create({ name: 'lucy' }),
-    ]);
+    // await conn.sync({ force: true });
+    // await Promise.all([
+    //     User.create({ name: 'moe' }),
+    //     User.create({ name: 'larry' }),
+    //     User.create({ name: 'lucy' }),
+    // ]);
 };
 
 init();
