ce6425f51332a4df37dbe7df5eb9f9660b99ebda
Matt Sze <msze400@gmail.com>
2021-02-27 09:36:32 -0500

    edit server file for heroku

--- server.js
+++ server.js
@@ -1,49 +1,45 @@
-const express = require('express')
-const { static } = express
-const path = require('path')
+const express = require('express');
+const { static } = express;
+const path = require('path');
 
-const app = express()
+const app = express();
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
 
-app.get('/', (req, res, next) =>
-    res.sendFile(path.join(__dirname, 'index.html'))
-)
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
 
-app.get('/api/users', async (req, res, next) => {
-    try {
-        res.send(await User.findAll())
-    } catch (ex) {
-        next(ex)
-    }
-})
+// app.get('/api/users', async (req, res, next) => {
+//     try {
+//         res.send(await User.findAll())
+//     } catch (ex) {
+//         next(ex)
+//     }
+// })
 
 const init = async () => {
     try {
-        await syncAndSeed()
-        const port = process.env.PORT || 3000
-        app.listen(port, () => console.log(`listening on port ${port}`))
+        await syncAndSeed();
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => console.log(`listening on port ${port}`));
     } catch (ex) {
-        console.log(ex)
+        console.log(ex);
     }
-}
+};
 
-const Sequelize = require('sequelize')
-const { STRING } = Sequelize
-const conn = new Sequelize(
-    process.env.DATABASE_URL || 'postgres://localhost/acme_db'
-)
+const Sequelize = require('sequelize');
+const { STRING } = Sequelize;
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db');
 
 const User = conn.define('user', {
     name: STRING,
-})
+});
 const syncAndSeed = async () => {
-    await conn.sync({ force: true })
+    await conn.sync({ force: true });
     await Promise.all([
         User.create({ name: 'moe' }),
         User.create({ name: 'larry' }),
         User.create({ name: 'lucy' }),
-    ])
-}
+    ]);
+};
 
-init()
+init();
