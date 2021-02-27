d44f2020e20ac40b5c85a6488ceecc1f4aab9fb1
Matt Sze <msze400@gmail.com>
2021-02-27 00:28:54 -0500

    center modal

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -74,6 +74,7 @@ export default function SimpleModal() {
                 onClose={handleClose}
                 aria-labelledby="simple-modal-title"
                 aria-describedby="simple-modal-description"
+                style={{ alignItems: 'center', justifyContent: 'center' }}
             >
                 {body}
             </Modal>
