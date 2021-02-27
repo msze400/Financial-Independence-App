2dbd029198fc4b02914943b8fc39ac92ddeb8844
Matt Sze <msze400@gmail.com>
2021-02-27 09:43:32 -0500

    edit server file for heroku

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const init = async () => {
     try {
         await syncAndSeed();
         const port = process.env.PORT || 3000;
-        app.listen(port, () => console.log(`listening on port ${port}`));
+        app.listen(process.env.PORT || port, () => console.log(`listening on port ${port}`));
     } catch (ex) {
         console.log(ex);
     }
